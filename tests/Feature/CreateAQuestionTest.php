6041e98bd59c41b814d4cd5c5eac71c38ddf093b
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-20 00:31:15 -0300

    test: should have at least 10 characters HEY-9

--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -2,7 +2,7 @@
 
 use App\Models\User;
 
-use function Pest\Laravel\{actingAs, assertDatabaseHas, post};
+use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, post};
 
 it('should be able to create a new question bigger than 255 characters', function () {
     // Arrange :: Preparar
@@ -23,6 +23,17 @@ it('should check if ends with question mark ?', function () {
     expect(true)->toBeTrue();
 })->todo();
 
-it('should have at least 10 characters', function () {
-    expect(true)->toBeTrue();
-})->todo();
+it('should have at least 10 characters when creating a new question', function () {
+    // Arrange :: Preparar
+    $user = User::factory()->create();
+    actingAs($user);
+
+    // Act :: Agir
+    $request = post(route('question.store'), [
+        'question' => str_repeat('*', 8) . '?',
+    ]);
+
+    //Assert :: Verificar
+    $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
+    assertDatabaseCount('questions', 0);
+});
