a10060b5f40322f6ffd54eb0af276fadf7de8032
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-20 00:52:19 -0300

    feat: should have at least 10 characters when creating a new question HEY-9

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use Closure;
 use Illuminate\Http\{RedirectResponse, Request};
 
 class QuestionController extends Controller
@@ -10,7 +11,15 @@ class QuestionController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $data = $request->validate([
-            'question' => ['required', 'min:10'],
+            'question' => [
+                'required',
+                'min:10',
+                function (string $attribute, mixed $value, Closure $fail) {
+                    if (!str($value)->endsWith('?')) {
+                        $fail('Are you sure that is a question? It is missing the question mark in the end.');
+                    }
+                },
+            ],
         ]);
 
         Question::query()->create(
