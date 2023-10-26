0a49beef332d87a8c7c6ba21615f8abf66733886
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-26 01:54:10 -0300

    feat: should make sure that only the person who has created the question can publish the question HEY-14

--- app/Http/Controllers/Question/PublishController.php
+++ app/Http/Controllers/Question/PublishController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Question;
 
 use App\Http\Controllers\Controller;
 use App\Models\Question;
-use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\{RedirectResponse, Response};
 
 class PublishController extends Controller
 {
     public function __invoke(Question $question): RedirectResponse
     {
+        $this->authorize('publish', $question);
         $question->update(['draft' => false]);
 
         return back();
--- app/Models/User.php
+++ app/Models/User.php
@@ -51,6 +51,11 @@ class User extends Authenticatable implements MustVerifyEmail
         return $this->hasMany(Vote::class);
     }
 
+    public function questions(): HasMany
+    {
+        return $this->hasMany(Question::class, 'created_by', 'id');
+    }
+
     public function like(Question $question): void
     {
         $this->votes()->updateOrCreate(
