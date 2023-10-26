6c953b9a98ab97168e0a9ae70efdd853e4853b6f
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-25 23:20:17 -0300

    refactor: remove lazy loading HEY-11

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -12,7 +12,7 @@
             <button class="text-green-500 flex items-start space-x-1">
                 <x-icons.thumbs-up class="w-5 h-5 hover:text-green-300 cursor-pointer" id="thumbs-up" />
                 <span>
-                    {{ $question->likes }}
+                    {{ $question->votes_sum_like ?? 0 }}
                 </span>
             </button>
         </x-form>
@@ -22,7 +22,7 @@
             <button class="text-red-500 flex items-start space-x-1">
                 <x-icons.thumbs-down class="w-5 h-5  hover:text-red-300 cursor-pointer" id="thumbs-down" />
                 <span>
-                    {{ $question->unlikes }}
+                    {{ $question->votes_sum_unlike ?? 0 }}
                 </span>
             </button>
         </x-form>
