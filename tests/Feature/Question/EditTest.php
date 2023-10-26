0a90f28e2ca2974a9f7715494e6c0f8f636e6a9c
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-26 16:53:36 -0300

    test: should return a view HEY-16

--- tests/Feature/Question/EditTest.php
+++ tests/Feature/Question/EditTest.php
@@ -16,3 +16,16 @@ it('should be able to open a question to edit', function () {
     //Assert :: Verificar
     $request->assertSuccessful();
 });
+
+it('should return a view', function () {
+    // Arrange :: Preparar
+    $user     = User::factory()->create();
+    $question = Question::factory()->create(['created_by' => $user->id, 'draft' => true]);
+    actingAs($user);
+
+    // Act :: Agir
+    $request = get(route('question.edit', $question));
+
+    //Assert :: Verificar
+    $request->assertViewIs('question.edit');
+});
