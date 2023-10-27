865542df47ba4b93c880eac14612c3a6753f0b5d
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-26 21:10:43 -0300

    test: should order by like and unlike, most liked question be at the top, most unliked questions should be in the bottom HEY-18

--- tests/Feature/Question/ListTest.php
+++ tests/Feature/Question/ListTest.php
@@ -33,3 +33,28 @@ test('should paginate the result', function () {
     //Assert :: Verificar
     $response->assertViewHas('questions', fn ($value) => $value instanceof LengthAwarePaginator);
 });
+
+test('should order by like and unlike, most liked question be at the top, most unliked questions should be in the bottom', function () {
+    // Arrange :: Preparar
+    $firstUser  = User::factory()->create();
+    $secondUser = User::factory()->create();
+    Question::factory()->count(5)->create();
+    $mostLikedQuestion   = Question::find(3);
+    $mostUnlikedQuestion = Question::find(1);
+    $firstUser->like($mostLikedQuestion);
+    $secondUser->unlike($mostUnlikedQuestion);
+    actingAs($firstUser);
+
+    // Act :: Agir
+    $response = get(route('dashboard'));
+
+    //Assert :: Verificar
+    $response->assertViewHas('questions', function ($questions) {
+        expect($questions)
+            ->first()->id->toBe(3)
+            ->and($questions)
+            ->last()->id->toBe(1);
+
+        return true;
+    });
+});
