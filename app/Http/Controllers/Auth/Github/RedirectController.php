8c2fc9ff3b7eac780880236a74894f3fdedd94a0
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-27 02:57:45 -0300

    feat: login using github HEY-25

---
+++ app/Http/Controllers/Auth/Github/RedirectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Github;
+
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class RedirectController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(): RedirectResponse
+    {
+        return Socialite::driver('github')->redirect();
+    }
+}
