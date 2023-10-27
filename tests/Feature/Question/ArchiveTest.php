794a8738f5ab3262f29e22e477991a1a17838864
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-26 21:50:19 -0300

    test: should make sure that only the person who has created the question can archive the question HEY-20

---
+++ tests/Feature/Question/ArchiveTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\{Question, User};
+
+use function Pest\Laravel\{actingAs, assertSoftDeleted, patch};
+
+it('should be able to archive a question', function () {
+    // Arrange :: Preparar
+    $user     = User::factory()->create();
+    $question = Question::factory()->create(['created_by' => $user->id, 'draft' => true]);
+    actingAs($user);
+
+    // Act :: Agir
+    $request = patch(route('question.archive', $question));
+
+    //Assert :: Verificar
+    $request->assertRedirect();
+    assertSoftDeleted('questions', ['id' => $question->id]);
+    expect($question)->refresh()->deleted_at->not->toBeNull();
+});
+
+it('should make sure that only the person who has created the question can archive the question', function () {
+    // Arrange :: Preparar
+    $rightUser = User::factory()->create();
+    $wrongUser = User::factory()->create();
+    $question  = Question::factory()->create(['created_by' => $rightUser->id, 'draft' => true]);
+    actingAs($wrongUser);
+
+    // Act :: Agir
+    $request = patch(route('question.archive', $question));
+
+    //Assert :: Verificar
+    $request->assertForbidden();
+
+    // Arrange :: Preparar
+    actingAs($rightUser);
+
+    // Act :: Agir
+    $request = patch(route('question.archive', $question));
+
+    //Assert :: Verificar
+    $request->assertRedirect();
+});
